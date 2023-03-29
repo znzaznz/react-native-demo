f4c6ae950f304421cbe28344477fee0f70ae61f6
xiaoznz <779740337@qq.com>
2023-03-29 15:42:52 +0800

    新增react-native-navigation

--- App.jsx
+++ App.jsx
@@ -8,53 +8,18 @@
 import React from 'react';
 import {StyleSheet, Text} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {Button, CheckBox, Icon} from '@rneui/base';
-import {createTheme, ThemeProvider} from '@rneui/themed';
-import 'react-native-vector-icons/FontAwesome';
-import * as Animatable from 'react-native-animatable';
+import {ThemeProvider} from '@rneui/themed';
+import {NavigationContainer} from '@react-navigation/native';
 
 function App() {
-  const [checked, setChecked] = React.useState(true);
-  const toggleCheckbox = () => setChecked(!checked);
-  const theme = createTheme({
-    lightColors: {
-      primary: '#f40202',
-    },
-    darkColors: {
-      primary: '#b71212',
-    },
-    mode: 'light',
-  });
-
   return (
-    <SafeAreaProvider>
-      <ThemeProvider theme={theme}>
-        <Icon name="rowing" />
-        <Animatable.Text
-          animation="slideInDown"
-          duration={1000}
-          iterationCount={'infinite'}
-          direction="alternate">
-          你好，抄袭了
-        </Animatable.Text>
-        <CheckBox checked disabled title="Label" />
-        <CheckBox
-          checked={checked}
-          checkedIcon="heart"
-          uncheckedIcon="heart-o"
-          checkedColor="red"
-          onPress={toggleCheckbox}
-        />
-        <CheckBox
-          checked={checked}
-          onPress={toggleCheckbox}
-          checkedIcon="dot-circle-o"
-          uncheckedIcon="circle-o"
-        />
-        <Text>大家好我是vue</Text>
-        <Button title={'My Button'} />
-      </ThemeProvider>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <ThemeProvider>
+          <Text>你好，写代码了</Text>
+        </ThemeProvider>
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 }
 
