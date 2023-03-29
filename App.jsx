c5dae7f30064ee833acef1df81949774e79ba3cf
xiaoznz <779740337@qq.com>
2023-03-29 15:49:51 +0800

    删删减减

--- App.jsx
+++ App.jsx
@@ -6,9 +6,9 @@
  */
 
 import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {Text} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {ThemeProvider} from '@rneui/themed';
+import {CheckBox, ThemeProvider} from '@rneui/themed';
 import {NavigationContainer} from '@react-navigation/native';
 
 function App() {
@@ -16,30 +16,12 @@ function App() {
     <NavigationContainer>
       <SafeAreaProvider>
         <ThemeProvider>
+          <CheckBox checked size={30} />
           <Text>你好，写代码了</Text>
         </ThemeProvider>
       </SafeAreaProvider>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
